50ddfefe1d510a53d7fb09e5983254f6a36e9e4d
Saumya Khobragade <khobragadesaumya@gmail.com>
2024-10-05 15:30:14 +0530

    Add files via upload

---
+++ ascii value.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+    char x;
+    printf("Enter a character:\n");
+    scanf("%c",&x);
+
+    printf("\nCharacter: %c\n",x);
+    printf("ASCII value: %d",x);
+}
